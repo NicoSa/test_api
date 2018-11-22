98759a974bb4723ee7ad58fde64accc742ccdc9f
NicoSa <nico@nicosaueressig.de>
2018-11-22 14:53:17 +0100

    quick hello world

---
+++ test/controllers/welcome_controller_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class WelcomeControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get welcome_index_url
+    assert_response :success
+  end
+
+   test "should get index again" do
+    get welcome_index_url
+    assert_response :success
+  end
+
+
+end
